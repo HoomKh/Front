86a9bb6feb60516a9198355431db902d7d82d93c
samim-sn <samim@greenhabitat.at>
2024-07-15 00:22:59 +0330

    feat: the best upgrade this app has ever seen by sahand

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -1,12 +1,13 @@
-import { Socials } from '@/constants';
+import { Socials } from '@/constants'
 import Image from 'next/image'
 import React from 'react'
 
-export const Navbar = () => {
+export const Navbar = () =>
+{
   return (
     <div className="w-screen h-[65px] fixed top-0 shadow-lg justify-between shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-10">
       <div className="w-full h-full flex flex-row items-center justify-between px-[10px]">
-        <a href="#about-me" className="h-auto w-auto flex flex-row items-center">
+        <a href="#about-me" className="h-auto w-auto flex flex-row items-center basis-1/3">
           <Image
             src="/Logo.png"
             alt="logo"
@@ -16,23 +17,23 @@ export const Navbar = () => {
           />
         </a>
 
-        <div className="flex-1 justify-center font-bold hidden md:flex">
+        <div className="flex-1 justify-center font-bold hidden md:flex basis-1/3">
           <div className="w-[500px] h-full flex flex-row items-center justify-evenly">
             <div className="flex items-center justify-around w-full h-auto px-[20px] py-[10px] rounded-full text-gray-200">
-              <a href="#about-me" className="cursor-pointer hover:border-b-1 hover:border-white">
+              <a href="#about-me" className="cursor-pointer hover:border-b-1 hover:border-white basis-1/3 text-center">
                 About me
               </a>
-              <a href="#skills" className="cursor-pointer hover:border-b-1 hover:border-white">
+              <a href="#skills" className="cursor-pointer hover:border-b-1 hover:border-white basis-1/3 text-center">
                 Skills
               </a>
-              <a href="#projects" className="cursor-pointer hover:border-b-1 hover:border-white">
+              <a href="#projects" className="cursor-pointer hover:border-b-1 hover:border-white basis-1/3 text-center">
                 Projects
               </a>
             </div>
           </div>
         </div>
 
-        <div className="flex flex-row gap-5 !text-base text-white">
+        <div className="flex flex-row gap-5 !text-base text-white basis-1/3 justify-end">
           {Socials.map((social) => (
             <a href={social.link} key={social.name} target="_blank" rel="noopener noreferrer">
               <social.src />
@@ -42,8 +43,8 @@ export const Navbar = () => {
       </div>
     </div>
 
-  );
-};
+  )
+}
 
 export default Navbar;
 
